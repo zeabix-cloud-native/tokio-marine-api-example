e9fc4cc03c9c1e061de7f3418f5a2c240232f941
Phakphoom-dev <68674095+Phakphoom-dev@users.noreply.github.com>
2024-06-04 12:54:32 +0700

    chore: add api spec description

--- obj/Debug/net8.0/TokioMarineApiExample.AssemblyInfo.cs
+++ obj/Debug/net8.0/TokioMarineApiExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TokioMarineApiExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c5cfd386789fdc02e6050793559896edfc394ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd475c2d90e6b0e189d393f340184a62be33563")]
 [assembly: System.Reflection.AssemblyProductAttribute("TokioMarineApiExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TokioMarineApiExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
